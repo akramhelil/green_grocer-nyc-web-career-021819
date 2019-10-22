c3af58c395a9c8e953d8a23b3f0a1f38e85f014f
Akram Helil <akramhelil@gmail.com>
2019-10-22 20:25:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,11 +17,25 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  hash = {}
+  cart.each do |item, detail|
+    coupons.each do |coupon|
+      if item == coupon[:item] && detail[:count]>=coupon[:num]
+        detail[:count]= detail[:count] - coupon[:num]
+          if hash["#{item} W/COUPON"]
+            hash["#{item} W/COUPON"][:count]+=1
+          else
+            hash["#{item} W/COUPON"]={price: coupon[:cost], clearance: detail[:clearance], count: 1}
+          end
+        end
+      end
+      hash[item] = detail
+    end
+    hash
 end
 
 def apply_clearance(cart)
-  # code here
+  
 end
 
 def checkout(cart, coupons)
